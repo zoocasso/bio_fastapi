44c8d67f43ac0668202542dfba855789a549d2f0
HyeongJu Lee <62836776+zoocasso@users.noreply.github.com>
2026-03-30 22:28:55 +0900

    Add files via upload

---
+++ bio_model_visualization/python/config.py
@@ -0,0 +1,9 @@
+DATABASE_CONFIG = {
+    'host': '211.34.105.108',
+    'user': 'root',
+    'password': 'vision9551',
+    'dbname': '__bio_db',
+}
+
+LDA_PARAM_CONFIG = {'NUM_TOPICS' : 40,
+                    'NUM_KEYWORD' : 50}
